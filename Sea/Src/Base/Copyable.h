4a1f340be8c568a416850079c307cb09feb89f2f
as981242002 <875229565@qq.com>
2018-12-05 23:20:20 +0800

    add copyable

---
+++ Sea/Src/Base/Copyable.h
@@ -0,0 +1,19 @@
+#ifndef SEA_BASE_COPYABLE_H_
+#define SEA_BASE_COPYABLE_H_
+
+namespace Sea
+{
+
+class Copyable
+{
+public:
+	Copyable() = default;
+	~Copyable() = default;
+
+private:
+
+};
+
+}//namespace Sea
+
+#endif // SEA_BASE_COPYABLE_H_
