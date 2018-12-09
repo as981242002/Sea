ed37619491c3f6932a5d58bad6fd28e3bb9870b2
as981242002 <875229565@qq.com>
2018-12-09 22:06:24 +0800

    add timezone

--- Sea/Src/Base/TimeZone.cpp
+++ Sea/Src/Base/TimeZone.cpp
@@ -6,6 +6,10 @@
 #include<cstdint>
 #include<cstdio>
 
+#ifdef _linux_
+#include <endian.h>
+#endif
+
 #include"../Base/Date.h"
 #include"../Base/NonCopyable.h"
 #include"../Base/TimeZone.h"
@@ -124,11 +128,61 @@ public:
 		return fp_;
 	}
 
+#ifdef _WIN32
+	//windows
 	string readBytes(int n)
 	{
-		auto buf = make_shared<char[]>(n);
-		
+		char* buf = new char(n);
+		size_t nr = ::fread_s(buf, n, 1, n, fp_);
+		if (nr != n)
+		{
+			throw logic_error("no enough data");
+		}
+		string s = buf;
+
+		delete buf;
+		return s;
 	}
+#endif // _WIN32
+
+#ifdef _linux_
+	string readBytes(int n)
+	{
+		char buf [n];
+		size_t nr = ::fread(buf, 1, n, fp_);
+		if (nr != n)
+		{
+			throw logic_error("no enough data");
+		}
+		string s = buf;
+
+		return string(buf, n);
+	}
+
+	int32_t readInt32()
+	{
+		int32_t x = 0;
+		size_t nr = ::fread(&x, 1, sizeof(int32_t), fp_);
+		if (nr != sizeof(int32_t))
+		{
+			throw logic_error("bad int32_t data");
+		}
+		return be32toh(x);
+	}
+
+	uint8_t readUInt8()
+	{
+		uint8_t x = 0;
+		ssize_t nr = ::fread(&x, 1, sizeof(uint8_t), fp_);
+		if (nr != sizeof(int32_t))
+		{
+			throw logic_error("bad uint8_t data");
+		}
+		return x;
+	}
+#endif // _linux_
+
+
 private:
 	FILE* fp_;
 };
